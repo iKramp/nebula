f5d2defc7b5022e27d73cf87fef881f907844d58
iKramp <nejc.susnik05@gmail.com>
2023-06-15 18:47:03 +0200

    default tokio-postgres config, except it uses the USERNAME env variable. a local postgresql server must be runnning on the pc with a database "testdb" for now

--- server/main.rs
+++ server/main.rs
@@ -1,5 +1,29 @@
 extern crate tokio_postgres;
+extern crate tokio;
+use tokio_postgres::{NoTls, Error};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main] // By default, tokio_postgres uses the tokio crate as its runtime.
+async fn main() -> Result<(), Error> {
+    // Connect to the database.
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=nejc dbname = testdb", NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await?;
+
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    assert_eq!(value, "hello world");
+
+    Ok(())
 }
