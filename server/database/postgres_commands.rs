f8720073f0da567bdefa5fc855af05c8dbcc51a0
iKramp <nejc.susnik05@gmail.com>
2023-06-20 09:26:00 +0200

    some basic commands, more will come soon

---
+++ server/database/postgres_commands.rs
@@ -0,0 +1,21 @@
+pub struct Command {
+    command: String,
+}
+
+impl Command {
+    pub fn select(columns: Vec<String>, table: String) -> Self {
+        Command {
+            command: format!("{} FROM {}", columns.join(", "), table)
+        }
+    }
+    
+    pub fn insert(columns: Vec<String>, items: Vec<Vec<String>>,  table: String) -> Self {
+        let mut elements = Vec::new();
+        for item in items.into_iter() {
+            elements.push(item.join(", "))    
+        }
+        Command {
+            command: format!("INSERT INTO {} ({}) VALUES ({})", table, columns.join(", "), elements.join("), ("))
+        }
+    }
+}
