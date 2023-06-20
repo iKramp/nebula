4b14ce5c7cf1c921ce5ec40f2573438d8d65e8c2
iKramp <nejc.susnik05@gmail.com>
2023-06-20 10:23:16 +0200

    didn't commit...

--- server/database/server_commands.rs
+++ server/database/server_commands.rs
@@ -1,6 +1,6 @@
 use super::postgres_commands::Command;
 
-pub fn add_message(user_id: u32, channel_id: u32, text: String, date_created: u64) -> String {
+pub fn save_message(user_id: u32, channel_id: u32, text: String, date_created: u64) -> String {
     let command = Command::insert(
         "user_id, channel_id, text, date_created".to_owned(),
         vec![vec![
