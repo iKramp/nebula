26973fe69b57d26d3dbe44af812d7cb28a9f1764
iKramp <nejc.susnik05@gmail.com>
2023-06-20 09:51:58 +0200

    cargo fmt

--- server/database/postgres_commands.rs
+++ server/database/postgres_commands.rs
@@ -5,22 +5,28 @@ pub struct Command {
 impl Command {
     pub fn select(columns: Vec<String>, table: String) -> Self {
         Command {
-            command: format!("{} FROM {}", columns.join(", "), table)
+            command: format!("{} FROM {}", columns.join(", "), table),
         }
     }
-    
-    pub fn insert(columns: Vec<String>, items: Vec<Vec<String>>,  table: String) -> Self {
+
+    pub fn insert(columns: Vec<String>, items: Vec<Vec<String>>, table: String) -> Self {
         let mut elements = Vec::new();
         for item in items.into_iter() {
-            elements.push(item.join(", "))    
+            elements.push(item.join(", "))
         }
         Command {
-            command: format!("INSERT INTO {} ({}) VALUES ({})", table, columns.join(", "), elements.join("), ("))
+            command: format!(
+                "INSERT INTO {} ({}) VALUES ({})",
+                table,
+                columns.join(", "),
+                elements.join("), (")
+            ),
         }
     }
 
     pub fn _where(mut self, condition: &str) -> Self {
-        self.command.push_str(format!(" WHERE {condition}").as_str());
+        self.command
+            .push_str(format!(" WHERE {condition}").as_str());
         self
     }
 }
