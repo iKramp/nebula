df4011d68922e7bb969fa7cd717f0dbef73d47be
iKramp <nejc.susnik05@gmail.com>
2023-06-20 12:06:07 +0200

    it will now return the message id for the clients

--- server/database/postgres_commands.rs
+++ server/database/postgres_commands.rs
@@ -3,13 +3,13 @@ pub struct Command {
 }
 
 impl Command {
-    pub fn select(columns: Vec<String>, table: String) -> Self {
+    pub fn select(columns: Vec<String>, table: &str) -> Self {
         Command {
             command: format!("SELECT {} FROM {}", columns.join(", "), table),
         }
     }
 
-    pub fn insert(columns: String, items: Vec<Vec<String>>, table: String) -> Self {
+    pub fn insert(columns: &str, items: Vec<Vec<String>>, table: &str) -> Self {
         let mut elements = Vec::new();
         for item in items.into_iter() {
             elements.push(item.join(", "))
@@ -27,6 +27,12 @@ impl Command {
     pub fn _where(mut self, condition: &str) -> Self {
         self.command
             .push_str(format!(" WHERE {condition}").as_str());
+        self
+    }
+
+    pub fn returning(mut self, field: &str) -> Self {
+        self.command
+            .push_str(format!(" RETURNING {field}").as_str());
         self
     }
 }
--- server/database/server_commands.rs
+++ server/database/server_commands.rs
@@ -2,14 +2,15 @@ use super::postgres_commands::Command;
 
 pub fn save_message(user_id: u32, channel_id: u32, text: String, date_created: u64) -> String {
     let command = Command::insert(
-        "user_id, channel_id, text, date_created".to_owned(),
+        "user_id, channel_id, text, date_created",
         vec![vec![
             user_id.to_string(),
             channel_id.to_string(),
             text,
             date_created.to_string(),
         ]],
-        "message".to_owned(),
-    );
+        "message",
+    )
+    .returning("id");
     command.command
 }
